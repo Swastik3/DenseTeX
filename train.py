e5c35e0cc312f2fdb648cf5830592200671bbec8
Idhant Gulati <idhant.gulati@gmail.com>
2024-07-10 01:59:55 -0400

    training cleanup

--- train.py
+++ train.py
@@ -21,28 +21,23 @@ import time
 import datetime
 import math
 from contextlib import nullcontext
-import random
-import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
-from torch.utils.data import DataLoader, Dataset  
+from torch.nn.utils.rnn import pad_sequence
 from model import GPTConfig, GPT
 from torchvision import models
 from DenseNet import PositionalEncoding2D, InputEmbeddings
-from transformers import AutoTokenizer
 from torchvision.models import DenseNet169_Weights
-from torch.utils.data import Subset
-from DataLoader import CustomDataset
-import tqdm
+from torcheval.metrics.functional import multiclass_f1_score
 from DataLoader import get_dataloader
 import wandb
+from torchtext.data.metrics import bleu_score
 
 # HYPERPARAMETERS
 
-# default config values designed to train a gpt2 (124M) on OpenWebText
-
+TORCH_LOGS="+dynamo"
 # max train time
 max_train_time = 3 # in mins
 # logging
@@ -58,13 +53,13 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = False # disabled by default
+wandb_log = True # disabled by default
 wandb_project = 'image2latex'
 wandb_run_name = 'run' + str(time.time())
 # data
 dataset = 'UniMER'
 gradient_accumulation_steps = 1 # 5 * 8 # used to simulate larger batch sizes
-batch_size = 5 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 1 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 300 # max token length
 # model
 n_layer = 12
@@ -90,16 +85,16 @@ backend = 'nccl' # 'nccl', 'gloo', etc.
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = True # use PyTorch 2.0 to compile the model to be faster
+# init these up here, can override if init_from='resume' (i.e. from a checkpoint)
+iter_num = 0
+best_val_loss = 1e9
 
-# --------------------------DO NOT CHANGE---------------------------------------
 # configuration parameters that are allowed to be overridden from command line
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
-# -----------------------------------------------------------------------------
 
 
-# --------------------------DO NOT CHANGE---------------------------------------
 # setting up the environment for distributed data parallel training 
 # various inits, derived attributes, I/O setup
 ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
@@ -136,18 +131,11 @@ ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=
 # -----------------------------------------------------------------------------
 
 
-# init these up here, can override if init_from='resume' (i.e. from a checkpoint)
-iter_num = 0
-best_val_loss = 1e9
-
 
 
 # model init
 model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=300,
                   bias=bias, dropout=dropout) # start with model_args from command line
-
-
-# --------------------------DO NOT CHANGE---------------------------------------
 
 # initialization of model based on the arg init_from (resume, scratch, gpt2, etc.)
 if init_from == 'scratch':
@@ -183,16 +171,6 @@ elif init_from == 'resume':
     iter_num = checkpoint['iter_num']
     best_val_loss = checkpoint['best_val_loss']
 
-
-# ----------------------------------------------------------------------------
-
-
-# # crop down the model block size if desired, using model surgery
-# if block_size < model.config.block_size:
-#     model.crop_block_size(block_size)
-#     model_args['block_size'] = block_size # so that the checkpoint will have the right value
-
-
 # move the model to the correct device
 model.to(device)
 
@@ -214,16 +192,6 @@ if compile:
 # wrap model into DDP container
 if ddp:
     model = DDP(model, device_ids=[ddp_local_rank])
-
-# ---------------------------------------------------------------------------
-
-def log_info(message, also_print=False):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"[{timestamp}] {message}"
-    with open(log_file, 'a', encoding='utf-8') as f:
-        f.write(log_message + '\n')
-    if also_print:
-        print(log_message)
 
 # Define the DenseNet169 model
 densenet_model = models.densenet169(weights=DenseNet169_Weights.IMAGENET1K_V1) # change to DEFAULT HERE
@@ -253,9 +221,8 @@ num_epochs = 3
 global_step = 0
 max_length = 300
 
-# INITALIZE THE TOKENIZER HERE !!!!!!
+# Load the tokenizer
 tokenizer = model.tokenizer
-# tokenizer = AutoTokenizer.from_pretrained("witiko/mathberta")
 
 def tokenize_latex(latex_text, max_length):
     toks = tokenizer(latex_text, padding='max_length', truncation=True, max_length=max_length, return_tensors='pt')
@@ -268,43 +235,31 @@ def tokenize_latex(latex_text, max_length):
     targets[:, -1] = tokenizer.pad_token_id
     
     return input_ids, attention_mask, targets
-
+# logging results to a txt file
+def log_info(message, also_print=False):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"[{timestamp}] {message}"
+    with open(log_file, 'a', encoding="utf-8") as f:
+        f.write(log_message + '\n')
+    if also_print:
+        print(log_message)
 
 # get the dataloader
 train_loader = get_dataloader(batch_size=batch_size, image_dir='./data/UniMER-1M/images', label_file='./data/UniMER-1M/train.txt')
 val_loader = get_dataloader(batch_size=batch_size, image_dir='./data/UniMER-Test/spe/', label_file='./data/UniMER-Test/spe.txt', cache_file='valid_indices_val.pkl')
 
-# get a very small subset of the entire dataset 
-# subset_size = 1024
-
-# def get_subset_dataloader(batch_size=batch_size, image_dir = '../data/UniMER-1M/images/', label_file = '../data/UniMER-1M/train.txt', subset_size = subset_size) :
-
-#     full_dataset = CustomDataset(image_dir = image_dir, label_file = label_file, transform = None, cache_file = 'valid_indices_cache.pkl')
-
-#     # random select a subset of indices
-#     subset_indices = random.sample(range(len(full_dataset)), subset_size)
-#     #create a subset dataset
-#     subset_dataset = Subset(full_dataset, subset_indices)
-#     # create a dataloader for the subset
-#     subset_dataloader = DataLoader(subset_dataset, batch_size = batch_size, shuffle = True)
-
-#     return subset_dataloader
-
-# # subset train_loader
-# train_loader = get_subset_dataloader(batch_size=batch_size, image_dir='../data/UniMER-1M/images/', label_file='../data/UniMER-1M/train.txt', subset_size=subset_size)
-
-
+max_n = 4 # max n-gram for BLEU score
 # Evaluation function
 @torch.no_grad()
 
 # calculate loss on val sets
 def evaluate(model , val_loader, device, eval_iters=eval_iters):
-
     model.eval()
 
     total_loss = 0
     num_batches = 0
-
+    total_bleu = 0
+    total_f1 = 0
     for i, (images, latex_labels) in enumerate(val_loader):
         if i >= eval_iters:
             break
@@ -318,19 +273,42 @@ def evaluate(model , val_loader, device, eval_iters=eval_iters):
         targets = targets.to(device)
         
         # Forward pass
+        gptconf = GPTConfig(**model_args)
         outputs = model(images=images, targets=targets)
         loss = outputs[1] if isinstance(outputs, tuple) else outputs.loss
+        if isinstance(outputs, tuple):
+            logits = outputs[0]
+        else :
+            logits = outputs
+        print("Logits", logits.shape)
+
+        tempbleu = 0; count = 0
+        micro_f1_score = 0
         
+        for logit, label in zip(logits, input_ids):
+            pred = torch.multinomial(logit.softmax(dim=-1), num_samples=1)
+            pred = pred[pred != tokenizer.pad_token_id]
+            label = label[label != tokenizer.pad_token_id]
+            padded_set = pad_sequence([pred,label], batch_first=True)
+            micro_f1_score += multiclass_f1_score(padded_set[0], padded_set[1])
+            predicted_tokens = tokenizer.convert_ids_to_tokens(pred)
+            decoded_label = tokenizer.convert_ids_to_tokens(label)
+            tempbleu += bleu_score([predicted_tokens], [[decoded_label]], max_n=max_n, weights=[1.0])
+            count +=1
+            
+        total_bleu += tempbleu / count
+        total_f1 += micro_f1_score / count
+        
+        loss = outputs[1] / gradient_accumulation_steps 
         total_loss += loss.item()
-        num_batches += 1
-    
+        num_batches += 1   
     avg_loss = total_loss / num_batches
+    avg_bleu = total_bleu / num_batches
+    avg_f1 = total_f1 / num_batches
     model.train()
+    print("avg bleu" , avg_bleu)
+    return avg_loss, avg_bleu, avg_f1
 
-    return avg_loss
-
-
-# ----------------- DO NOT CHANGE -------------------------------------
 # learning rate decay scheduler (cosine with warmup)
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
@@ -345,12 +323,9 @@ def get_lr(it):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
     return min_lr + coeff * (learning_rate - min_lr)
 
-
 # logging
 if wandb_log and master_process:
     wandb.init(project=wandb_project, name=wandb_run_name, config=config)
-
-# -------------------------------------------------------------------------
 
 # training loop 
 t0 = time.time()
@@ -358,13 +333,9 @@ local_iter_num = 0 # number of iterations in the lifetime of this process
 raw_model = model.module if ddp else model # unwrap DDP container if needed
 running_mfu = -1.0
 
-
-# max_batches = 100
-
 model = CombinedModel(densenet_model, model)
 model.train()
 train_start_time = time.time()
-
 
 for epoch in range(num_epochs):
     log_info(f"Starting epoch {epoch+1}/{num_epochs}", also_print=True)
@@ -373,13 +344,8 @@ for epoch in range(num_epochs):
         if time.time() - train_start_time > max_train_time * 60:  # Convert minutes to seconds
             log_info(f"Reached maximum training time of {max_train_time} minutes. Stopping training.", also_print=True)
             break
-    # for batch_idx, (images, latex_labels) in enumerate(tqdm.tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}")):
 
-        # if batch_idx >= max_batches:
-        #     break
-        
         # Get the image embeddings and the latex labels
-
         images = images.to(device)
         
         # Tokenize LaTeX labels
@@ -391,7 +357,6 @@ for epoch in range(num_epochs):
         
         # Determine and set the learning rate for this iteration
         lr = get_lr(iter_num) if decay_lr else learning_rate
-
 
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
@@ -405,7 +370,6 @@ for epoch in range(num_epochs):
             print(f"step {iter_num}: val loss {val_loss:.4f}")
             model.train()
 
-            
             if wandb_log:
                 wandb.log({
                     "iter": iter_num,
@@ -431,14 +395,7 @@ for epoch in range(num_epochs):
         
         # Forward backward update, with optional gradient accumulation
         for micro_step in range(gradient_accumulation_steps):
-
             if ddp:
-            
-            # in DDP training we only need to sync gradients at the last micro step.
-            # the official way to do this is with model.no_sync() context manager, but
-            # I really dislike that this bloats the code and forces us to repeat code
-            # looking at the source of that context manager, it just toggles this variable
-
                 model.require_backward_grad_sync = (micro_step == gradient_accumulation_steps - 1)
             
             # Forward pass
@@ -452,8 +409,6 @@ for epoch in range(num_epochs):
                 sample_prediction = torch.multinomial(logits[0].softmax(dim=-1), num_samples=1)
                 non_pad_mask = sample_prediction != tokenizer.pad_token_id
                 decoded_prediction = tokenizer.decode(sample_prediction[non_pad_mask])
-                #print(f"Sample non-padded prediction: {decoded_prediction}")
-                #print(f"Actual label: {latex_labels[0]}")
                 loss = outputs[1] / gradient_accumulation_steps 
 
             # Backward pass
@@ -472,22 +427,16 @@ for epoch in range(num_epochs):
         scaler.update()
         optimizer.zero_grad(set_to_none=True)
 
-        
         # Timing and logging
         t1 = time.time()
         dt = t1 - t0
         t0 = t1
-
 
         if iter_num % log_interval == 0 and master_process:
             # get loss as float. note: this is a CPU-GPU sync point
             # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
             lossf = loss.item() * gradient_accumulation_steps
             
-            # if local_iter_num >= 5 :
-                # mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
-                # running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-            # print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
             print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms")
 
             log_info(f"Iteration {iter_num}, Epoch {epoch+1}, Batch {batch_idx+1}:")
@@ -504,8 +453,6 @@ for epoch in range(num_epochs):
             log_info(f"Sample prediction at iteration {iter_num}:")
             log_info(f"  Predicted: {decoded_prediction}")
             log_info(f"  Actual: {latex_labels[0]}")
-
-        # ... (keep existing code for optimization steps)
 
         # Update the logging for the end of each iteration
         if iter_num % log_interval == 0 and master_process:
@@ -525,7 +472,6 @@ for epoch in range(num_epochs):
         break
 
 print(f"Training completed. Total iterations: {iter_num}")
-
 
 if ddp:
     destroy_process_group()
