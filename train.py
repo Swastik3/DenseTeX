79d6eb810eda7041e9e19831a51de13db4e75243
NishkalHundia <nishkalnhundia@gmail.com>
2024-07-22 15:27:23 +0530

    final params for training

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ sample_interval = 100  # Log sample predictions every 100 iterations
 detailed_log_interval = 100  # Log detailed metrics every 10 iterations
 # I/O
 out_dir = 'out'
-eval_interval = 1000
+eval_interval = 1200
 log_interval = 20
 eval_iters = 90
 eval_only = False # if True, script exits right after the first eval
@@ -66,8 +66,8 @@ wandb_project = 'image2latex'
 wandb_run_name = 'run' + str(time.time())
 # data
 dataset = 'UniMER'
-gradient_accumulation_steps = 2*4 # used to simulate larger batch sizes
-batch_size = 8 # if gradient_accumulation_steps > 1, this is the MICRO-BATCH SIZE
+gradient_accumulation_steps = 8*4 # used to simulate larger batch sizes
+batch_size = 8   # if gradient_accumulation_steps > 1, this is the MICRO-BATCH SIZE
 block_size = 300 # max token length
 # model
 n_layer = 12
@@ -77,15 +77,15 @@ dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 800000 # total number of training iterations
+max_iters = 60000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 5000 # how many steps to warm up for
-lr_decay_iters = 650000 # should be ~= max_iters per Chinchilla
+warmup_iters = 1000 # how many steps to warm up for
+lr_decay_iters = 45000 # should be ~= max_iters per Chinchilla
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
