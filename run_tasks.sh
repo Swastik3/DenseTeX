b87c68fdd4e12df79eed0cd34ec8b4182a601cbd
marmik <103449011+MarmikChaudhari@users.noreply.github.com>
2024-07-22 15:13:17 +0530

    Update run_tasks.sh

--- run_tasks.sh
+++ run_tasks.sh
@@ -3,17 +3,19 @@
 # Clone the GitHub repository
 sudo apt-get install -y unzip
 
+# get the dataset
 mkdir data
 cd data
 wget https://huggingface.co/datasets/wanderkid/UniMER_Dataset/resolve/main/UniMER-Test.zip
 wget https://huggingface.co/datasets/wanderkid/UniMER_Dataset/resolve/main/UniMER-1M.zip
 unzip UniMER-Test.zip &
 unzip UniMER-1M.zip
-
 cd ..
 
+# install pip
+sudo apt update
+sudo apt install python3-pip
+
+# install requirements
 pip install -r requirements.txt
-
-
-
-
+pip install torchtext torcheval
